7d2427c409e6871b8d19d1d18437cbcb1346a116
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-27 00:02:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+
+post '/piglatinize' do
+  @new_pig_latin = PigLatinizer.new(params[:user_phrase]).piglatinize
+  erb :piglatinize
+end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,32 @@
+require 'pry'
+class PigLatinizer
+
+  attr_accessor :text, :array
+  attr_reader :pig_latin
+
+  def initialize(text = "")
+    @text = text
+  end
+
+  def piglatinize(text = @text)
+    @text = text
+    if !@text.include?(" ")
+      @pig_latin = @text.slice(1, @text.length - 1) + @text[0] + "ay"
+    else
+      @array = @text.split(" ")
+      @array.map! do |word|
+      if word[0].match(/[aeiouAEIOU]/)
+        word = word + "lay"
+      else 
+        word = word.slice(1, word.length - 1) + word[0] + "ay"
+      end 
+      end
+      @pig_latin = @array.join(" ")
+
+    end
+    @text = @pig_latin
+    @pig_latin
+
+  end
+
+end
